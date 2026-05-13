457dd2083096f156df25a750ffdbacc6e787efda
dimplessithole35-collab <>
2026-05-13 23:47:57 +0000

    No content

---
+++ mastering_python_if_else_statements/no_content/main.py
@@ -0,0 +1,9 @@
+steps_taken = 8500
+step_goal = 10000 
+
+result_more_than_90 = False  # Default 
+if steps_taken >=step_goal *0.9:
+    print("Great job! You've reached more than 90% of your goal!")
+else:
+    steps_left = step_goal - steps_taken
+    print(f"Keep going! You still need {steps_left} more steps to reach your goal.")
