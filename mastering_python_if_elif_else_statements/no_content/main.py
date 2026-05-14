7a3159e32b808d2d275e698b6130a9a4cad92c42
dimplessithole35-collab <>
2026-05-14 15:33:41 +0000

    No content

---
+++ mastering_python_if_elif_else_statements/no_content/main.py
@@ -0,0 +1,16 @@
+workout_duration = 45
+message = ""
+
+# Check if the workout duration is more than 60 minutes
+if workout_duration>60:
+    message = "Excellent job! You've worked out for over an hour!"
+# Check if the workout duration is between 30 and 60 minutes
+elif 30<=workout_duration<=60:
+    message = "Great job! Keep it up!"
+# If the workout duration is less than 30 minutes 
+elif workout_duration<30:
+    message = "Try to aim for at least 30 minutes of exercise."
+else :
+    message = "get on with your workout"
+# Testing
+print("Exercise feedback:", message)
